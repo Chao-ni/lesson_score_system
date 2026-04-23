249711564a90eacaacfe678b397076de75b61b27
Chao-ni <96013962+Chao-ni@users.noreply.github.com>
2026-04-23 13:35:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -50,13 +50,25 @@ SCORE_DIMENSIONS = SCORING_RULES["评分维度"]
 #     )
 #
 #     return conn
+# def get_db():
+#     conn = pymysql.connect(
+#         host=os.environ.get("centerbeam.proxy.rlwy.net"),
+#         user=os.environ.get("root"),
+#         password=os.environ.get("fyqcyclFddWXgBhTaAuUkLhbClCDLaYu"),
+#         database=os.environ.get("railway"),
+#         charset="utf8mb4"
+#     )
+#     return conn
+CURRENT_TABLE = "scores_round2"
 def get_db():
     conn = pymysql.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME"),
-        charset="utf8mb4"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        charset="utf8mb4",
+        autocommit=False
     )
     return conn
 
